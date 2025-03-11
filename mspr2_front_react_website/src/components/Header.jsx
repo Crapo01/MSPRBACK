ce1e7cc9348db7b9fbf605f868da819193e5748b
Crapo01 <capusluc@gmail.com>
2025-03-11 13:22:20 +0100

    updated header with icons

--- mspr2_front_react_website/src/components/Header.jsx
+++ mspr2_front_react_website/src/components/Header.jsx
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Button, Image } from 'react-bootstrap';
 import authService from '../services/auth.service';
-
+import { FaSignOutAlt, FaSignInAlt ,FaUserPlus} from "react-icons/fa";
 function Header() {
 
   const [currentUser, setCurrentUser] = useState(undefined);
@@ -21,10 +21,14 @@ function Header() {
   }
 
   return (
-    <div >
-      <Navbar expand="lg" className={"mb-5 p-3 border rounded bg-dark"}>
+    <div className="mb-5 p-3 border rounded bg-dark">
+      
+      <Navbar expand="lg" className=" justify-content-between">
+        
         <Navbar.Brand ><Link to={"/"}><Image src="/images/logo_festival.png" alt="logo nation sound" width={"150px"} rounded /></Link></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-light" />
+        
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="bg-light"  />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto ">
             <div className='d-flex justify-content-end flex-column'>
@@ -35,44 +39,42 @@ function Header() {
                 <Nav.Link href="https://nationsoundmetal.rf.gd/wp/partenaires/"><div className='font-weight-bolder text-light text-decoration-none h5'>PARTENAIRES</div></Nav.Link>
                 <Nav.Link href="https://nationsoundmetal.rf.gd/wp/foire-aux-questions/"><div className='font-weight-bolder text-light text-decoration-none h5'>FAQ</div></Nav.Link>
                 <Nav.Link href="https://nationsoundmetal.rf.gd/wp"><div className='font-weight-bolder text-light text-decoration-none h5'>BOUTIQUE</div></Nav.Link>
-              </div>
-              <div className='ms-auto'>
+              </div> 
+              <div className='d-flex  justify-content-end mb-2 '>
                 {currentUser ? (
-                  <div className="navbar-nav justify-content-end">
-                    <li className="nav-item bg-light m-1 rounded">
-                      <Link to={"/profile"} className="nav-link">
+                  <div className="navbar-nav justify-content-end flex-row">
+                    <li className="nav-item  m-1 ">
+                      <Link to={"/profile"} className="nav-link text-light">
                         {currentUser.username}
                       </Link>
                     </li>
                     <li className="nav-item bg-light m-1 rounded">
-                      <a href="/login" className="nav-link" onClick={logOut}>
-                        LogOut
-                      </a>
+                    <a href="/login" className="nav-link px-2" onClick={logOut}>
+                <FaSignOutAlt size={25}  className="text-dark" /> {/* Icône Logout */}
+              </a>
                     </li>
                   </div>
                 ) : (
-                  <div className="navbar-nav justify-content-end">
-                    <li className="nav-item bg-light m-1 rounded">
-                      <Link to={"/login"} className="nav-link">
-                        Login
-                      </Link>
-                    </li>
-
-                    <li className="nav-item bg-light m-1 rounded">
-                      <Link to={"/register"} className="nav-link">
-                        Sign Up
-                      </Link>
-                    </li>
-                  </div>
+                  <div className="navbar-nav justify-content-end flex-row">
+          <li className="nav-item bg-light m-1 rounded">
+            <Link to={"/login"} className="nav-link px-2">
+              <FaSignInAlt size={25} className="text-dark" />
+            </Link>
+          </li>
+          <li className="nav-item bg-light m-1 rounded">
+            <Link to={"/register"} className="nav-link px-2">
+              <FaUserPlus size={25} className="text-dark" />
+            </Link>
+          </li>
+        </div>
                 )}
               </div>
-              
-
             </div>
           </Nav>
         </Navbar.Collapse>
+       
       </Navbar>
-    </div>
+     </div>
   );
 };
 export default Header;
